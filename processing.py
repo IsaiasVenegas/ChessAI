a220242099ae52d4548e8680b06fc28799e69d56
IsaiasVenegas <isaias.venegas@ing.uchile.cl>
2021-11-20 17:43:45 +0100

    Source square is not coded. Updated algorithm for disambiguation cases, castling and multiple special moves like capture and check. TODO: promotions

--- processing.py
+++ processing.py
@@ -13,9 +13,10 @@ def check_color(n_mov, pow):
 
 
 """
-Checks if the move was a normal move, capture (x), check (+),
-mate (#) or castling (O-O or O-O-O)
-returning a value and removing the corresponding symbol
+Returns 1 for normal move
+        2 for castling (O-O or O-O-O)
+    or  3 for captures (x)
+Removes the corresponding symbol, including check (+) and mate (#) 
 Arguments:
 - n_mov: turn in which the move was played
 - move: current move
@@ -25,30 +26,34 @@ def parse_special_move(n_mov, move, pow):
     char_remove = ''
     char_replace = ''
     value = 1
-    if "x" in move:
-        char_remove = "x"
-        if check_color(n_mov, 0) == 1:
-            # white piece movement
-            # TODO
-            source_row = -1  # pawn
-        else:
-            # black piece movement
-            # TODO
-            source_row = 1  # pawn
-        char_replace = str(int(move[-1])+source_row)
-        value = 2
-    elif "+" in move:
-        char_remove = "+"
-        value = 3
-    elif "#" in move:
-        char_remove = "#"
-        value = 4
-    elif "O-O-O" in move:
+    if "O-O-O" in move:
         char_remove = "O-O-O"
-        value = 5
+        if(check_color(n_mov, 0)==1):
+            char_replace = "Rc1"
+        else:
+            char_replace = "Rc8"
+        value = 2
     elif "O-O" in move:
         char_remove = "O-O"
-        value = 5
+        if(check_color(n_mov, 0)==1):
+            char_replace = "Rg1"
+        else:
+            char_replace = "Rg8"
+        value = 2
+    move = move.replace(char_remove, char_replace)
+    char_remove = ''
+    char_replace = ''
+    if "x" in move:
+        source_col = move[move.find("x")-1]
+        if(source_col.islower()):
+            char_remove = source_col
+        char_remove += "x"
+        value = 3
+        move = move.replace(char_remove, char_replace)
+    if "+" in move:
+        char_remove = "+"
+    elif "#" in move:
+        char_remove = "#"
     move = move.replace(char_remove, char_replace)
     return move, value*10**pow
 
@@ -83,31 +88,32 @@ def parse_column(pos, pow):
 
 """
 Returns chess movements in a numerical notation
-following   
-    color|piece |movement|sourceCol|sourceRow|targetCol|targetRow       
-    {1,2}|{1,,6}|{1,,4}  |{0,,8}   |{0,,8}   |{1,,8}   |{1,,8}
+following
+    color|piece |movement|targetCol|targetRow       
+    {1,2}|{1,,6}|{1,,4}  |{1,,8}   |{1,,8}
 structure and values, removing one character after each checkup
-ONLY the capture movements indicate their source.
 Arguments:
 - game: list of moves in string format
 """
 def parse_movements(game):
     movements = []
     for n_mov in range(0, len(game)):
+        print(n_mov)
         mov = game[n_mov]
         mov, code = parse_special_move(n_mov, mov, 4)
-        if len(mov) != 0:  # castling
-            code += parse_piece(mov[0], 5)
-            if code//(10**5) != 1:
-                # is not a pawn
+        code += parse_piece(mov[0], 5)
+        if code//(10**5) != 1:
+            # is not a pawn
+            mov = mov[1:]
+            # disambiguation case
+            while(len(mov)>2):
                 mov = mov[1:]
-            while len(mov) > 0:
-                pow = len(mov)-1
-                code += parse_column(mov[0], pow)
-                mov = mov[1:]
-                code += int(mov[0]) * 10**(pow-1)
-                mov = mov[1:]
-
-            code += check_color(n_mov, 6)
-            movements.append(code)
+        while len(mov) > 0:
+            pow = len(mov)-1
+            code += parse_column(mov[0], pow)
+            mov = mov[1:]
+            code += int(mov[0]) * 10**(pow-1)
+            mov = mov[1:]
+        code += check_color(n_mov, 6)
+        movements.append(code)
     return movements
